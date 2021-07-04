0a28879b2b2e0d67116bdb61311b9b3918197411
Ezequiel H <horvitzezequiel@gmail.com>
2021-07-04 03:04:52 +0200

    Creation of Portfolio Manager Page

---
+++ src/components/portfolio/portfolio-detail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function(props) {
+  return (
+    <div>
+      <h2>Porfiolio details for {props.match.params.slug}</h2>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+}
