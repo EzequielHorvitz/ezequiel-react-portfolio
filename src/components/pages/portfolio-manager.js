14db7561fd4cdb0431bd8d405e64ce7cd3f11a81
Ezequiel H <horvitzezequiel@gmail.com>
2021-07-05 05:07:30 +0200

    Sidebar completed, added handlers

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,11 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: [],
+    };
+
+    this.handleSuccesfulFormSubmission =
+      this.handleSuccesfulFormSubmission.bind.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
+
+  handleSuccesfulFormSubmission(portfolioItem) {}
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://ezequielh.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items],
+        });
+      })
+      .catch((error) => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
-      <div> 
-          <h1> Porfolio Manager</h1>
-          <h1> Porfolio Manager</h1>
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm
+            handleSuccesfulFormSubmission={this.handleSuccesfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
+        </div>
+
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class PorfolioForm extends Component {
+  render() {
+    return (
+      <div>
+          <h1>portfolio form</h1>
+          <h1>portfolio form</h1>
+          
+      </div>
+    );
+  }
+}
